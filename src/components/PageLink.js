3d532a4ce93ef1ffc23f851b74f1cc62f8899abd
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-09-11 10:03:39 +0300

    added iteration 1

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 const PageLink = ({ link, itemClass }) => {
   return (
-    <li key={link.id}>
-      <a href={link.href} className={itemClass}>
+    <li>
+      <Link to={link.href} className={itemClass}>
         {link.text}
-      </a>
+      </Link>
     </li>
-  )
-}
-export default PageLink
+  );
+};
+
+export default PageLink;
